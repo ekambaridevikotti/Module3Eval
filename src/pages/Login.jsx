ff542f9555634e84cd3bda981face5eb6d1225d0
ekambaridevikotti <ekambaridevi123@gmail.com>
2026-01-03 19:03:02 +0530

    Added login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    const role = login(email, password);
+    if (!role) {
+      alert("Invalid credentials");
+    } else {
+      navigate(
+        role === "admin"
+          ? "/admin/dashboard"
+          : "/customers/dashboard"
+      );
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
